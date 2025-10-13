061b5de502491f8584fa0b2cbf32d1a7b734bc83
Lokesh <hanswork@gmail.com>
2025-10-13 12:46:53 +0530

    intigrating tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl  font-bold underline">
         Hello world!
       </h1>
     </>
